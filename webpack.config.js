0a8408c4a0e6b5795c91b305fbd8605a3dd56fc5
gracejoo26@gmail.com <gracejoo26@gmail.com>
2022-07-05 00:09:09 +0900

    dev environment error

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,38 @@
 const path = require("path");
-const TerserWebpackPlugin= require('terser-webpack-plugin');
+const TerserPlugin= require('terser-webpack-plugin');
+const HtmlwebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 module.exports={
   entry:"./src/js/index.js",
   output:{
     filename: "bundle.js",
     path: path.resolve(__dirname,"./dist"),
-    clean:true
+    clean: true
   },
-  devtool:"source-map",
-  mode:"development",
+  devtool: "source-map",
+  mode: "development",
+  plugin:[
+    new HtmlwebpackPlugin({
+      title:"keyboard",
+      template:"./index.html",
+      inject:"body",
+      favicon:"./favicon.ico"
+    }),
+    new MiniCssExtractPlugin({filename:"style.css"})
+  ],
+  module:{
+    rules:[
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
   optimization:{
     minimizer:[
-      new TerserWebpackPlugin()
+      new TerserPlugin(),
+      new CssMinimizerPlugin()
     ]
   }
 }
